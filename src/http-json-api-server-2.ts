dd92c10d6e595197c19d70ecb64215230e1001d1
Jaume Blanco González <jaumeblancog@gmail.com>
2024-07-16 20:47:26 +0200

    v0.2.0 - Completed part two of the exercise

--- src/http-json-api-server-2.ts
+++ src/http-json-api-server-2.ts
@@ -2,10 +2,10 @@ const http13 = require("http");
 const url = require("url");
 
 http13.createServer((req: any, res: any) => {
-    const newUrl = new URL(req.url, "http://example.com");
+    const newUrl = new URL(req.url, "http://localhost:3000");
     let urlPathName = newUrl.pathname; 
     let urlTime: any = newUrl.searchParams.get("iso");
-    let stringTime = new Date(urlTime)
+    let stringTime = new Date(urlTime);
     
     let time: any;
 
@@ -25,45 +25,12 @@ http13.createServer((req: any, res: any) => {
     let result = JSON.stringify(time);
     res.end(result)
     
-}).listen(process.argv[2]);
+}).listen(3000);
+
+http13.get(process.argv[2], (response: any) => {
+  response.on("data", (data: any) => {
+      console.log(data.toString());
+  })
+})
 
 
-  /* SOLUCIÓN OFICIAL .JS
-
-      'use strict'
-    const http = require('http')
-
-    function parsetime (time) {
-      return {
-        hour: time.getHours(),
-        minute: time.getMinutes(),
-        second: time.getSeconds()
-      }
-    }
-
-    function unixtime (time) {
-      return { unixtime: time.getTime() }
-    }
-
-    const server = http.createServer(function (req, res) {
-      const parsedUrl = new URL(req.url, 'http://example.com')
-      const time = new Date(parsedUrl.searchParams.get('iso'))
-      let result
-
-      if (/^\/api\/parsetime/.test(req.url)) {
-        result = parsetime(time)
-      } else if (/^\/api\/unixtime/.test(req.url)) {
-        result = unixtime(time)
-      }
-
-      if (result) {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(result))
-      } else {
-        res.writeHead(404)
-        res.end()
-      }
-    })
-    server.listen(Number(process.argv[2]))
-
-    */
--- src/http-uppercaserer-2.ts
+++ src/http-uppercaserer-2.ts
@@ -1,38 +1,30 @@
 const http12 = require("http");
-const map = require("through2-map");
+const fs12 = require("fs");
+
+const options = {
+  method: "POST",
+  port: process.argv[2]
+}
 
 http12.createServer((req: any, res: any) => {
-    if (req.method !== "POST") return res.end("Send me a POST\n")
-
-    req.pipe(map((chunk: any) => {
-      return chunk.toString().toUpperCase()
-    })).pipe(res);
-
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  const stream = fs12.createReadStream("./lectura.txt");
+  stream.pipe(res)  
 }).listen(process.argv[2]);
 
-http12.post("http://localhost:" + process.argv[2] + "/", (response: any) => {
-  response.on("data", (data: any) => {
-      console.log(data.toString());
-  })
+http12.get(options, (res: any) => {
+  let body: string = "";
+
+  if (options.method  === "POST") {
+  res.on("data", (chunk: any) => {
+    body += chunk.toString().toUpperCase();
+  });
+
+  res.on("end", () => {
+    console.log(body)
+  });
+
+  } else {
+    console.error("Send me a POST request")
+  }
 })
-
-
-/* SOLUCIÓN OFICIAL .JS
-
-    'use strict'
-    const http = require('http')
-    const map = require('through2-map')
-
-    const server = http.createServer(function (req, res) {
-      if (req.method !== 'POST') {
-        return res.end('send me a POST\n')
-      }
-
-      req.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase()
-      })).pipe(res)
-    })
-
-    server.listen(Number(process.argv[2]))
-
-*/    
