f4a939b8a1fd9fa7ff51cf5786bb2c81be7ff88f
Jaume Blanco González <jaumeblancog@gmail.com>
2024-07-18 11:05:01 +0200

    Delete learnyounode/dist directory

--- learnyounode/dist/app/baby-steps.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let result = 0;
-let i;
-for (i = 2; i < process.argv.length; i++) {
-    result += Number(process.argv[i]);
-}
-console.log(result);
-//# sourceMappingURL=baby-steps.js.map
--- learnyounode/dist/app/my-first-io.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const fs = require('fs');
-const buf = fs.readFileSync(process.argv[2]);
-const str = buf.toString().split('\n').length - 1;
-console.log(str);
-//# sourceMappingURL=my-first-io.js.map
